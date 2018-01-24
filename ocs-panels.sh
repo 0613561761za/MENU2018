fa88d6eed7bbde34a1f64d95812149fdbf0d3292
lnwshop <33618273+lnwshop@users.noreply.github.com>
2018-01-24 15:39:24 +0700

    Update ocs-panels.sh

--- ocs-panels.sh
+++ ocs-panels.sh
@@ -61,14 +61,14 @@ chown -R mysql:mysql /var/lib/mysql/
 chmod -R 755 /var/lib/mysql/
 
 apt-get install -y nginx php5 php5-fpm php5-cli php5-mysql php5-mcrypt
-rm /etc/nginx/sites-enabled/default
-rm /etc/nginx/sites-available/default
-mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup
-mv /etc/nginx/conf.d/vps.conf /etc/nginx/conf.d/vps.conf.backup
-wget -O /etc/nginx/nginx.conf "http://script.hostingtermurah.net/repo/blog/ocspanel-debian7/nginx.conf"
-wget -O /etc/nginx/conf.d/vps.conf "http://script.hostingtermurah.net/repo/blog/ocspanel-debian7/vps.conf"
-sed -i 's/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini
-sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
+#rm /etc/nginx/sites-enabled/default
+#rm /etc/nginx/sites-available/default
+#mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup
+#mv /etc/nginx/conf.d/vps.conf /etc/nginx/conf.d/vps.conf.backup
+#wget -O /etc/nginx/nginx.conf "http://script.hostingtermurah.net/repo/blog/ocspanel-debian7/nginx.conf"
+#wget -O /etc/nginx/conf.d/vps.conf "http://script.hostingtermurah.net/repo/blog/ocspanel-debian7/vps.conf"
+#sed -i 's/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini
+#sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 
 useradd -m vps
 mkdir -p /home/vps/public_html
